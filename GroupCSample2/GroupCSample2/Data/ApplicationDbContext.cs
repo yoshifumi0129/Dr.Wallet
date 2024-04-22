4563e47646468510619da102057c34cbe650268b
Fujii <Fujii>
2024-04-22 14:43:57 +0900

    新規登録

---
+++ GroupCSample2/GroupCSample2/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroupCSample2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<GroupCSample2.Models.Balance> Balances { get; set; } = default!;
+        public DbSet<GroupCSample2.Models.Category> Category { get; set; } = default!;
+        public DbSet<GroupCSample2.Models.UserAccount> UserAccount { get; set; } = default!;
+    }
+}
---
+++ GroupCSample2/GroupCSample2/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using GroupCSample2.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GroupCSample2.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        //カテゴリーの種類(日用品など…)
+        public string? CategoryName { get; set; }
+    }
+}
